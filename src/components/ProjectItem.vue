7806624b933c193ec4476c46cd9043428081d0e7
R S <r.stevenson@runbox.com>
2024-08-03 12:33:25 +1000

    restructure Splide carousel

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <SplideSlide>
+      <div class="column">
+        <div class="col">
+          <img :src="`src/assets/${props.image}`" style="max-height: 100%; max-width: 100%" alt="Sample 1">
+        </div>
+        <div class="col">
+          {{ props.description }}
+        </div>
+      </div>
+    </SplideSlide>
+  </template>
+  
+  <script setup>
+  import { SplideSlide } from '@splidejs/vue-splide';
+  defineOptions({
+    name: 'ProjectItem'
+  })
+  
+  const props = defineProps({
+    image: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    demoLink: {
+      type: String,
+      default: ''
+    },
+    sourceLink: {
+      type: String,
+      default: ''
+    }
+  })
+  </script>
+  
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,68 +1,21 @@
 <template>
   <q-page>
     <Splide ref="splide" :options="options" aria-label="My Favorite Images">
-    <SplideSlide>
-      <div class="column">
-        <div class="col">
-          <img src="~assets/jamming.jpg" style="max-height: 100%; max-width: 100%" alt="Sample 1">
-        </div>
-        <div class="col">
-          wertwertwert
-        </div>
-      </div>
-    </SplideSlide>
-    <SplideSlide>
-      <img src="~assets/jamming.jpg" style="max-height: 100%; max-width: 100%" alt="Sample 2">
-    </SplideSlide>
-    <SplideSlide>
-      <img src="~assets/dwitter.jpg" style="max-height: 100%; max-width: 100%" alt="Sample 3">
-    </SplideSlide>
-    <SplideSlide>
-      <img src="~assets/stats.jpg" style="max-height: 100%; max-width: 100%" alt="Sample 4">
-    </SplideSlide>
-  </Splide>
-
-    <!-- <q-carousel
-      animated
-      v-model="slide"
-      arrows
-      infinite
-      swipeable
-      control-color="primary"
-      control-type="flat"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      height="600px"
-    >
-      <q-carousel-slide :name="1" style="background-color: green;">
-        <div class="column" style="height: 568px">
-          <div class="col-4 col-md-6">.col-4 .col-md-6</div>
-          <div class="col-4 col-md-6">.col-4 .col-md-6</div>
-          <div class="col-4 col-md-6">.col-4 .col-md-6</div>
-          <div class="col-4 col-md-6">.col-4 .col-md-6</div>
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide :name="2" style="background-color: blue;">
-        <div class="column" style="height: 568px">
-          <div class="col-4 col-md-6">slide2</div>
-          <div class="col-4 col-md-6">slide2</div>
-          <div class="col-4 col-md-6">slide2</div>
-          <div class="col-4 col-md-6">slide2</div>
-        </div>
-      </q-carousel-slide>
-    </q-carousel> -->
+      <ProjectItem v-for="item in projectList" :key="item.image" v-bind="item" />
+    </Splide>
   </q-page>
 </template>
 
 <script lang="ts">
 import '@splidejs/vue-splide/css';
-import { Splide, SplideSlide, Options } from '@splidejs/vue-splide';
+import { Splide, Options } from '@splidejs/vue-splide';
 import { defineComponent } from 'vue';
+import ProjectItem from 'components/ProjectItem.vue'
 
 export default defineComponent({
   components: {
     Splide,
-    SplideSlide,
+    ProjectItem
   },
 
   setup() {
@@ -77,15 +30,34 @@ export default defineComponent({
       pagination: false
       // gap   : '1rem',
     };
+    const projectList = [
+    {
+      image: 'jamming.jpg',
+      description: 'jamming description',
+      demoLink: '',
+      sourceLink: ''
+    },
+    {
+      image: 'dwitter.jpg',
+      description: 'dwitter description',
+      demoLink: '',
+      sourceLink: ''
+    },
+    {
+      image: 'jamming.jpg',
+      description: 'satellite description',
+      demoLink: '',
+      sourceLink: ''
+    },
+    {
+      image: 'stats.jpg',
+      description: 'stats description',
+      demoLink: '',
+      sourceLink: ''
+    }
+  ]
 
-    return { options };
+    return { options, projectList };
   }
 });
-// export default {
-//   setup () {
-//     return {
-//       slide: ref(1)
-//     }
-//   }
-// }
 </script>
